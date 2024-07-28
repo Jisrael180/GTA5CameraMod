5c0c79461124a0c28f4d48b6080d50518366adee
James Israel <jisrael180@gmail.com>
2024-07-28 13:09:45 -0400

    Initial commit with project files

---
+++ CameraLensSwitcher.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using GTA;
+using GTA.Native;
+using GTA.Math;
+using NativeUI;
+
+public class CameraLensSwitcher : Script
+{
+    private Camera cam;
+    private bool isCameraActive = false;
+    private bool isTimecycleEnabled = true;
+    private int currentLens = 0;
+    private readonly float[] fovs = { 30.0f, 50.0f, 90.0f, 120.0f, 10.0f, 140.0f, 85.0f, 45.0f, 200.0f, 15.0f };
+    private readonly string[] lensNames = { "Telephoto", "Standard", "Wide-Angle", "Fisheye", "Macro", "Extreme Wide-Angle", "Portrait", "Super Telephoto", "Ultra Wide", "Tilt-Shift" };
+    private readonly string[] timecycleMods = { "custom_telephoto", "custom_standard", "custom_wideangle", "custom_fisheye", "custom_macro", "custom_extreme_wideangle", "custom_portrait", "custom_super_telephoto", "custom_ultrawide", "custom_tiltshift" };
+
+    private MenuPool menuPool;
+    private UIMenu mainMenu;
+    private UIMenuCheckboxItem timecycleToggle;
+
+    public CameraLensSwitcher()
+    {
+        Tick += OnTick;
+        KeyDown += OnKeyDown;
+
+        menuPool = new MenuPool();
+        mainMenu = new UIMenu("Camera Lens Switcher", "OPTIONS");
+
+        var activateDeactivateCamera = new UIMenuItem("Activate/Deactivate Camera");
+        var switchLens = new UIMenuItem("Switch Lens");
+        timecycleToggle = new UIMenuCheckboxItem("Depth of Field", true);
+
+        activateDeactivateCamera.Activated += (menu, item) => {
+            if (isCameraActive)
+            {
+                DeactivateCamera();
+            }
+            else
+            {
+                ActivateCamera();
+            }
+        };
+
+        switchLens.Activated += (menu, item) => {
+            SwitchLens();
+        };
+
+        timecycleToggle.CheckboxEvent += (sender, checkedState) => {
+            isTimecycleEnabled = checkedState;
+            if (!isTimecycleEnabled)
+            {
+                Function.Call(Hash.CLEAR_TIMECYCLE_MODIFIER);
+            }
+            else if (isCameraActive)
+            {
+                Function.Call(Hash.SET_TIMECYCLE_MODIFIER, timecycleMods[currentLens]);
+            }
+        };
+
+        mainMenu.AddItem(activateDeactivateCamera);
+        mainMenu.AddItem(switchLens);
+        mainMenu.AddItem(timecycleToggle);
+
+        mainMenu.RefreshIndex();
+        menuPool.Add(mainMenu);
+
+        ShowNotification("CameraLensSwitcher script loaded successfully.");
+    }
+
+    private void ActivateCamera()
+    {
+        try
+        {
+            if (!isCameraActive)
+            {
+                Ped playerPed = Game.Player.Character;
+                Vector3 pos = playerPed.Position + playerPed.ForwardVector * -1.0f + new Vector3(0, 0, 0.5f); // Default position
+                cam = World.CreateCamera(pos, playerPed.Rotation, fovs[currentLens]);
+                World.RenderingCamera = cam;
+                isCameraActive = true;
+                ShowNotification("Camera activated with " + lensNames[currentLens] + " Lens");
+
+                if (isTimecycleEnabled)
+                {
+                    Function.Call(Hash.SET_TIMECYCLE_MODIFIER, timecycleMods[currentLens]);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            ShowNotification("ActivateCamera failed: " + ex.Message);
+        }
+    }
+
+    private void DeactivateCamera()
+    {
+        try
+        {
+            if (isCameraActive)
+            {
+                World.RenderingCamera = null;
+                cam.Delete();
+                isCameraActive = false;
+                ShowNotification("Camera mode deactivated");
+
+                Function.Call(Hash.CLEAR_TIMECYCLE_MODIFIER);
+            }
+        }
+        catch (Exception ex)
+        {
+            ShowNotification("DeactivateCamera failed: " + ex.Message);
+        }
+    }
+
+    private void SwitchLens()
+    {
+        try
+        {
+            if (isCameraActive)
+            {
+                currentLens = (currentLens + 1) % fovs.Length;
+                cam.FieldOfView = fovs[currentLens];
+                ShowNotification("Switched to " + lensNames[currentLens] + " Lens");
+
+                if (isTimecycleEnabled)
+                {
+                    Function.Call(Hash.SET_TIMECYCLE_MODIFIER, timecycleMods[currentLens]);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            ShowNotification("SwitchLens failed: " + ex.Message);
+        }
+    }
+
+    private void ShowNotification(string message)
+    {
+        GTA.UI.Notification.PostTicker(message, false, false);
+    }
+
+    private void OnTick(object sender, EventArgs e)
+    {
+        menuPool.ProcessMenus();
+
+        if (isCameraActive)
+        {
+            HandleCameraMovement();
+
+            Ped playerPed = Game.Player.Character;
+            if (currentLens == 4) // Macro lens
+            {
+                cam.Position = playerPed.Position + playerPed.ForwardVector * -2.0f + new Vector3(0, 0, 2.0f); // High isometric view for Macro
+            }
+            else
+            {
+                cam.Position = playerPed.Position + playerPed.ForwardVector * -1.0f + new Vector3(0, 0, 0.5f); // Default position
+            }
+
+            cam.Rotation = playerPed.Rotation;
+
+            if (Game.Player.Character.IsWalking || Game.Player.Character.IsRunning)
+            {
+                cam.Position = playerPed.Position + playerPed.ForwardVector * -1.5f + new Vector3(0, 0.5f, 0.5f);
+                cam.Rotation = playerPed.Rotation;
+            }
+        }
+    }
+
+    private void OnKeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+    {
+        if (e.KeyCode == System.Windows.Forms.Keys.F7)
+        {
+            mainMenu.Visible = !mainMenu.Visible;
+        }
+    }
+
+    private void HandleCameraMovement()
+    {
+        // Allow the player to move the camera using the right analog stick (assuming a controller is used)
+        float camSpeed = 0.1f;
+
+        if (Game.IsControlPressed(GTA.Control.MoveUpOnly))
+        {
+            cam.Position += cam.ForwardVector * camSpeed;
+        }
+        if (Game.IsControlPressed(GTA.Control.MoveDownOnly))
+        {
+            cam.Position -= cam.ForwardVector * camSpeed;
+        }
+        if (Game.IsControlPressed(GTA.Control.MoveLeftOnly))
+        {
+            cam.Position -= cam.RightVector * camSpeed;
+        }
+        if (Game.IsControlPressed(GTA.Control.MoveRightOnly))
+        {
+            cam.Position += cam.RightVector * camSpeed;
+        }
+        if (Game.IsControlPressed(GTA.Control.MoveUp))
+        {
+            cam.Position += cam.UpVector * camSpeed;
+        }
+        if (Game.IsControlPressed(GTA.Control.MoveDown))
+        {
+            cam.Position -= cam.UpVector * camSpeed;
+        }
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,53 @@
+Congratulations on downloading the "Camera Lens Switcher" mod, and thus validating my lack of a social life.
+
+This mod was created to help GTA-V's machinima making community have a little more flexibility when it comes to their cinematography, by allowing you to switch out the game's typical camera with a camera designed to replicate popular lenses used in film making. 
+Do you fancy yourself a Christopher Nolan type? The 50mm might be for you. You could get your Stanley Kubrick on with an 18mm wide angle. Heck, you can get extra artsy with our 70mm tilt-shift lens. We have 10 different lenses to choose from, and 10 different depth of field settings to match said lenses.
+
+I am putting this thing up on GitHub under the repository name GTA5CameraMod, so if you have additional lenses you want to add, the source code is up there.
+
+This is my first attempt at creating a mod, but I have it on good authority that this mod should not cause your computer to explode. 
+
+I hope that this mod helps you make great art that you can be proud of. 
+
+Note: The lenses are not great for actual gameplay (the macro and tilt-shift are particularly difficult). This is made with machinima in mind, so be sure to have what you are going to record planned ahead of time 'cause you might be a little blind. Possibly good to set to a task via the Menyoo mod, or using the Scene Director mod. Menyoo's spooner mode also retain's the camera lens, so if you want to preview how it will look in Rockstar Editor, you can use the spooner mode to move the camera wherever you like.
+
+REQUIREMENTS:
+ScriptHookV
+ScriptHookVDotNet
+NativeUI
+Open IV or Codewalker
+
+INSTALLING:
+Put file into your Grand Theft Auto V\scripts\ directory. If the scripts folder does not exist, create it (though, assuming you have all the above requirements, it would be weird if you didn't already have a scripts folder, right?)
+
+
+Open your GTA V directory using OpenIV or Codewalker.
+Navigate to \update\update.rpf\common\data\timecycle\ .
+Enable Edit Mode in OpenIV.
+Replace timecycle_mods_1.xml with the modified timecycle_mods_1.xml file provided in this mod. Ensure you have a backup of the original timecycle_mods_1.xml file because that file is fragile and you don't want to mess it up. 
+
+HOW TO USE
+Activate Camera Lens Mod:
+
+Press F7 to activate the camera lens mod. You can then click "switch lens" to cycle through the different lenses. You can also click to toggle off the depth-of-field settings for each lens.
+
+
+Telephoto : ~85mm
+Standard : ~50mm
+Wide-Angle : ~18mm
+Fisheye : ~8mm
+Macro : ~200mm (macro lenses are more about close focus distance than FOV, but this is an approximation)
+Extreme Wide-Angle : ~7mm
+Portrait : ~35mm
+Super Telephoto : ~300mm
+Ultra Wide : ~120mm
+Tilt-Shift : ~70mm
+
+Credits
+ScriptHookV: Developed by Alexander Blade
+ScriptHookVDotNet: Developed by Crosire
+NativeUI: Developed by Guad
+ChatGPT for teaching me how to make this ... 
+
+SPECIAL THANKS
+YouTube creator whanowa -- the man is the GOAT. He has helped keep GTA5 machinima community alive. Check out his channel here: https://www.youtube.com/@whanowa
